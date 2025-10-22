a7ab4086f4265a55e7e249aaafac31e94989a566
SivaKarthik <sivakarthiks@brahmarsive.com>
2025-10-22 22:27:36 +0530

    sceneBoundingMesh to set camera target and positioning

--- src/classes/MyScene.js
+++ src/classes/MyScene.js
@@ -1,4 +1,4 @@
-import {Engine, Scene, Vector3, ArcRotateCamera, AppendSceneAsync, LoadAssetContainerAsync} from "@babylonjs/core";
+import {Engine, Scene, Vector3, ArcRotateCamera, AppendSceneAsync, LoadAssetContainerAsync, BoundingInfo, MeshBuilder} from "@babylonjs/core";
 import "@babylonjs/loaders/glTF"
 
 class MyScene{
@@ -14,6 +14,7 @@ class MyScene{
         this.camera = new ArcRotateCamera("camera1", 0, 0, 10, new Vector3(0, 5, -10), this.scene);
         this.camera.setTarget(Vector3.Zero());
         this.camera.attachControl(this.canvas, true);
+        this.camera.minZ = 0;
     };
 
     static getInstanceOfMyScene(canvas){
@@ -27,7 +28,6 @@ class MyScene{
         //await AppendSceneAsync(glbFile, this.scene);
         this.container = await LoadAssetContainerAsync(glbFile, this.scene);
         this.container.addAllToScene();
-        console.log(this.scene.meshes);
     }
 
     async clearSceneMeshes(){
@@ -45,15 +45,68 @@ class MyScene{
 
     createEnvironment(environmentName){
         //1. calculate overall bounding info
-        //2. set min and max zoom based on that
+        this.calculateBoundingInfo();
+
+        if(this.sceneBoundingInfo){
+            //4. focus the bounding info center
+            this.camera.setTarget(this.sceneBoundingInfo.boundingBox.centerWorld);
+            //6. set camera position
+            this.camera.radius = this.sceneBoundingInfo.boundingSphere.radius * 2.5;
+            //2. set min and max zoom based on bb
+            this.camera.upperRadiusLimit = this.sceneBoundingInfo.boundingSphere.radius * 25;
+            this.camera.lowerRadiusLimit = this.sceneBoundingInfo.boundingSphere.radius;
+            //5. set scroll speed based on bb
+            //this.camera.wheelPrecision = this.sceneBoundingInfo.boundingSphere.radius * 50;
+        }
         //3. create environment with dimensions based on bounding info
-        //4. focus the bounding info center
+
         const env = this.scene.createDefaultEnvironment({
             environmentTexture: "./assets/Studio_Softbox_2Umbrellas_cube_specular.env",
             createSkybox: true, 
             skyboxTexture: "./assets/Studio_Softbox_2Umbrellas_cube_specular.env",
             createGround: false,
         });
+    }
+
+    calculateBoundingInfo(){
+        this.sceneBoundingInfo = null;
+
+        const sceneMeshes = this.container.meshes.filter(mesh => {
+            const meshMin = mesh.getBoundingInfo().boundingBox.minimumWorld;
+            const meshMax = mesh.getBoundingInfo().boundingBox.maximumWorld;
+            return !(meshMin.equals(meshMax));
+        });
+
+        if(sceneMeshes.length == 0)
+            return;
+        
+        let spaceMin = sceneMeshes[0].getBoundingInfo().boundingBox.minimumWorld;
+        let spaceMax = sceneMeshes[0].getBoundingInfo().boundingBox.maximumWorld;
+        sceneMeshes.forEach(mesh => {
+            const meshMin = mesh.getBoundingInfo().boundingBox.minimumWorld;
+            const meshMax = mesh.getBoundingInfo().boundingBox.maximumWorld;
+            spaceMin = Vector3.Minimize(spaceMin, meshMin);
+            spaceMax = Vector3.Maximize(spaceMax, meshMax);
+        });
+
+        if(spaceMin.equals(spaceMax))
+            return;
+        
+        this.sceneBoundingInfo = new BoundingInfo(spaceMin, spaceMax);
+
+        // const boundingMesh = MeshBuilder.CreateBox("sceneBoundingBox", {
+        //     width: tempBoundingInfo.boundingBox.maximumWorld.x - tempBoundingInfo.boundingBox.minimumWorld.x,
+        //     height: tempBoundingInfo.boundingBox.maximumWorld.y - tempBoundingInfo.boundingBox.minimumWorld.y,
+        //     depth: tempBoundingInfo.boundingBox.maximumWorld.z - tempBoundingInfo.boundingBox.minimumWorld.z,
+        // }, this.scene);
+
+        // boundingMesh.position.copyFrom(tempBoundingInfo.boundingBox.centerWorld)
+        // boundingMesh.visibility = 0;
+        // boundingMesh.isPickable = false;
+        // //boundingMesh.showBoundingBox = true;
+        // this.sceneBoundingBox = boundingMesh;
+
+        console.log(this.sceneBoundingInfo);
     }
 
     onRender(){
